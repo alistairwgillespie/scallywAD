b6b0960790b9a232c341091c6dbe2a56c6a2e0b9
Alistair Gillespie <alistair.gillespie@au.ey.com>
2019-12-11 09:21:01 +1100

    Initial setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='scallywAD',
+    version='0.1dev',
+    packages=['scallywad',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
